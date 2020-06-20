761422ccfee38ce1f0d6aaef30d98f6bdda527c4
EsraaElNaggar <esraamelnaggar@hotmail.com>
2020-06-21 01:55:32 +0200

    👽 add error page

---
+++ src/components/error.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import _ from "lodash";
+
+import Nav from './nav';
+
+class ErrorPage extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Nav
+                    className="white"
+                />
+                <main className='covidUnivers'>
+                    <React.Fragment>
+                        {_.times(80, () => (
+                            <img className='covid' src="/assets/covid.png" />
+                        ))}
+                    </React.Fragment>
+                    <article className='covidHelpMsg'>
+                        <p>Damnit stranger,</p>
+                        <p>You got lost in the <strong>404 Covid</strong> galaxy.</p>
+                        <p>
+                            <button>Put your mask & Go back.</button>
+                        </p>
+                    </article>
+                </main>
+            </React.Fragment>
+        );
+    }
+}
+
+export default ErrorPage;
