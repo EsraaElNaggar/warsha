c2ce2e04ac50acbe053756b75aba09769ac2832d
Moon04 <30897431+Moon04@users.noreply.github.com>
2020-06-21 18:05:23 +0200

    customer interface / appointment confirmation component

---
+++ src/components/customer/appointment_confirmation/booking-thanks.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const BookingThanks = props => {
+   
+    return ( 
+        <React.Fragment>
+            <div className="thanks">
+                <p className="text-center">
+                    Thank you for using <i className="c-green">Warsha</i>, Catch roads again soon!
+                </p>
+                <p className="text-center">
+                    If you have any inquiry please call
+                    <i className="fas fa-phone-alt c-green" />
+                    16576
+                </p>
+            </div>
+        </React.Fragment>
+    );
+    
+};
+ 
+export default BookingThanks;
