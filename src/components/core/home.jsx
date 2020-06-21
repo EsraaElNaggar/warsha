abc3d7ffab7e47a2eb0e8cc2e1a9993f7b2931c3
Moon04 <30897431+Moon04@users.noreply.github.com>
2020-06-21 21:21:30 +0200

    changes in structure

--- src/components/core/home.jsx
+++ src/components/core/home.jsx
@@ -14,17 +14,17 @@ export default function Home() {
             <section class="full-height-container credits">
                 <section class="why-us">
                     <section>
-                        <img src="/HomeImages/Search.svg" />
+                        <img src="/HomeImages/Search.svg" alt=""/>
                         <h3>Search</h3>
                         <p>Search for Mechanics, plumbers ... etc. in your area</p>
                     </section>
                     <section>
-                        <img src="/HomeImages/Mechanic.svg" />
+                        <img src="/HomeImages/Mechanic.svg" alt=""/>
                         <h3>Compare</h3>
                         <p>Compare between mulitple choices and choose what suits you</p>
                     </section>
                     <section>
-                        <img src="/HomeImages/Book.svg" />
+                        <img src="/HomeImages/Book.svg" alt=""/>
                         <h3>Book</h3>
                         <p>Book your appointment, with no need to wait</p>
                     </section>
