5ff39460414743ae3db5bde3095d2f45661f23d7
unknown <maryamtselim@gmail.com>
2020-06-21 12:07:58 +0200

    home,aboutus,filter added

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,27 +4,27 @@ export default function Home() {
     return (
         <>
             <section class="banner">
-                <article class="banner-shape"></article>
-                <article class="banner-introduction">
+                <article class="Talal-banner-shape"></article>
+                <article class="Talal-banner-introduction">
                     <div><span>Find</span> the best mechanics in Egypt
-            </div> <i class="fas fa-angle-down angle-down"></i>
+            </div> <i class="fas fa-angle-down Talal-angle-down"></i>
                 </article>
-                <article class="banner-image"></article>
+                <article class="Talal-banner-image"></article>
             </section>
             <section class="full-height-container credits">
                 <section class="why-us">
                     <section>
-                        <img src="./Search.svg" />
+                        <img src="/HomeImages/Search.svg" />
                         <h3>Search</h3>
                         <p>Search for Mechanics, plumbers ... etc. in your area</p>
                     </section>
                     <section>
-                        <img src="./Mechanic.svg" />
+                        <img src="/HomeImages/Mechanic.svg" />
                         <h3>Compare</h3>
                         <p>Compare between mulitple choices and choose what suits you</p>
                     </section>
                     <section>
-                        <img src="./Book.svg" />
+                        <img src="/HomeImages/Book.svg" />
                         <h3>Book</h3>
                         <p>Book your appointment, with no need to wait</p>
                     </section>
