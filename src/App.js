c5c24d41db296de6d571c30d078a66adc6fb4e67
unknown <maryamtselim@gmail.com>
2020-06-21 20:28:31 +0200

    dropdown added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import Filter from './components/filter';
-import Home from './components/home';
+import AboutUs from './components/aboutus';
+import Dropdown from './components/dropdown';
 function App() {
+  let carBrands = ["Audi","BMW","Ford","Lamborgini","Ferrari"];
+  let carModel = ["blah", "blah1","blah2","blah3"]
+  let carYear = [2010, 2011 ,2012,2013]
   return (
-    <Home/>
+    <>
+    <Dropdown header={"HKi"} list={carBrands}/>
+    </>
   );
 }
 
--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -1,6 +1,53 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-export default function Filter() {
+export default function Filter(props) {
+    let { carBrands, carModel,carYear,avalibility } = props;
+    //carBrands Element
+    carBrands = carBrands.map(c => {
+        return (
+            <li key={c}>
+                <input type="radio" name="car-make" value={c} id={c} />
+                <label htmlFor={c}>{c}</label>
+            </li>
+        )
+
+    })
+    carModel = carModel.map(c => {
+        return (
+            <li key={c}>
+                <input type="checkbox" name="car-model" value={c} id={c} />
+                <label htmlFor={c}>{c}</label>
+            </li>
+        )
+
+    })
+    carYear = carYear.map(c => {
+        return (
+            <li key={c}>
+                <input type="checkbox" name="car-year" value={c} id={c} />
+                <label htmlFor={c}>{c}</label>
+            </li>
+        )
+
+    })
+    avalibility = avalibility.map(c => {
+        return (
+            <li key={c}>
+                <input type="checkbox" name="avalibility" value={c} id={c} />
+                <label htmlFor={c}>{c}</label>
+            </li>
+        )
+
+    })
+
+    const menu = useRef(null);
+    //Model Element
+    let toggleList = (e) => {
+        e.stopPropagation();
+        e.target.parentNode.childNodes[1].classList.toggle("collapse-filter-menu")
+        console.log()
+
+    }
     return (
         <div className="col-3">
             <div className="search-filters">
@@ -10,47 +57,53 @@ export default function Filter() {
                 </p>
                 <ul className="filters p-0 m-0">
                     <li>
-                        <i className="fas fa-cogs pr-2"></i>
-                        Make
-                        <i className="fas fa-caret-down filter-arrow"></i>
-
-
+                        <div onClick={toggleList}>
+                            <span> <i className="fas fa-cogs pr-2"></i>
+                            Make
+                            </span>
+                            <i className="fas fa-caret-down filter-arrow"></i>
+                        </div>
                         <ul className="filter-options">
-                            <li>
-                                <input type="checkbox" name="car-make" id="audi" />
-                                Audi
-                            </li>
-                            <li>
-                                <input type="checkbox" name="car-make" id="bmw" />
-                                BMW
-                            </li>
-                            <li>
-                                <input type="checkbox" name="car-make" id="ford" />
-                                Ford
-                            </li>
-                            <li>More</li>
+                            {carBrands}
                         </ul>
-
                     </li>
                     <li>
-                        <i className="fas fa-car pr-2"></i>
-                        Model
-                        <i className="fas fa-caret-right filter-arrow"></i>
+                        <div onClick={toggleList}>
+                            <span> <i className="fas fa-car pr-2"></i>
+                        Model </span>
+                            <i className="fas fa-caret-down filter-arrow"></i>
+                        </div>
+                        <ul className="filter-options">
+                            {carModel}
+                        </ul>
                     </li>
                     <li>
-                        <i className="far fa-calendar pr-2"></i>
-                            Year
-                        <i className="fas fa-caret-right filter-arrow"></i>
+                        <div onClick={toggleList}>
+                            <span> <i className="fas fa-calendar pr-2"></i>
+                        Year </span>
+                            <i className="fas fa-caret-down filter-arrow"></i>
+                        </div>
+                        <ul className="filter-options">
+                        {carYear}
+                        </ul>
                     </li>
                     <li>
-                        <i className="far fa-calendar-alt pr-2"></i>
-                            Availability
-                        <i className="fas fa-caret-right filter-arrow"></i>
+                    <div onClick={toggleList}>
+                            <span> <i className="fas fa-calendar-alt pr-2"></i>
+                        Avalibility </span>
+                            <i className="fas fa-caret-down filter-arrow"></i>
+                        </div>
+                        <ul className="filter-options">
+                        {avalibility}
+                        </ul>
                     </li>
                     <li>
-                        <i className="fas fa-tools pr-2"></i>
-                            Remote Check
-                        <i className="fas fa-caret-right filter-arrow"></i>
+                    <div onClick={toggleList}>
+                            <span> <i className="fas fa-tools pr-2"></i>
+                            Remote </span>
+                            <input type="checkbox"/>
+                        </div>
+                        
                     </li>
                 </ul>
             </div>
