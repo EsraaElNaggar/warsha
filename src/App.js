69363c658e5c4cfd85589e97d2dd26ef011b0223
EsraaElNaggar <esraamelnaggar@hotmail.com>
2020-06-22 02:13:13 +0200

    ✅ test sign up for Center

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
-import AddReviewUser from "./components/addReviewUser";
-import AddAppointmentCenter from "./components/addAppointmentCenter";
-import ChangePasswordUser from "./components/changePasswordUser";
-import EditProfileUser from "./components/editProfileUser";
-import ContactUS from "./components/contactUs";
-import EditProfileCenter from "./components/editProfileCenter";
-import ReviewsCenter from "./components/reviewsCenter";
-import AppointmentsUser from "./components/appointmentsUser";
+
+// import AddReviewUser from "./components/addReviewUser";
+// import AddAppointmentCenter from "./components/addAppointmentCenter";
+// import ChangePasswordUser from "./components/changePasswordUser";
+// import EditProfileUser from "./components/editProfileUser";
+// import ContactUS from "./components/contactUs";
+// import EditProfileCenter from "./components/editProfileCenter";
+// import ReviewsCenter from "./components/reviewsCenter";
+// import AppointmentsUser from "./components/appointmentsUser";
+import SignupCenter from "./components/signupCenter";
+
 
 function App() {
   return (
     <React.Fragment>
-      <h1>working</h1>
-      <AppointmentsUser></AppointmentsUser>
+      <SignupCenter></SignupCenter>
     </React.Fragment>
   );
 }
