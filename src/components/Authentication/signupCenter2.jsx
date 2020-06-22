b7b70a029e291592a9796a00d807c58a05c120e4
EsraaElNaggar <esraamelnaggar@hotmail.com>
2020-06-22 02:17:56 +0200

    🌐 change login/signup locations

--- src/components/Authentication/signupCenter2.jsx
+++ src/components/Authentication/signupCenter2.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Input from './common/input';
+import Input from '../common/input';
 
 class SignupCenter2 extends Component {
     state = {
--- src/components/Authentication/signupUser3.jsx
+++ src/components/Authentication/signupUser3.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Input from './common/input';
+import Input from '../common/input';
 
 class SignUpUser3 extends Component {
     state = {
