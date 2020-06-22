5fc37559491889b9c4c2e15f5a946168d5ba2bcf
EsraaElNaggar <esraamelnaggar@hotmail.com>
2020-06-22 20:05:55 +0200

    🎨 inhance style

--- src/components/center/appointments_day/appointment.jsx
+++ src/components/center/appointments_day/appointment.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 
 const Appointment = props => {
-    return ( 
+    return (
         <React.Fragment>
             <div className="d-flex justify-content-between client-appointment">
                 {/* Appointment Cliend Card Start */}
                 <div className="appointment-client-card">
                     <div className="appointment-client-details">
-                    <span>
-                        Mona Mohamed
+                        <span>
+                            Mona Mohamed
                     </span>
-                    <span>
-                        12:00 PM
+                        <span>
+                            12:00 PM
                     </span>
                     </div>
                     <div className="client-card-btns d-flex">
-                    <button>
-                        {/* <i class="fas fa-user-check"></i> Checked In Icon */}
-                        <i className="fas fa-user-cog" />
+                        <button className="checkInBtn">
+                            {/* <i class="fas fa-user-check"></i> Checked In Icon */}
+                            <i className="fas fa-user-cog" />
                         Check In
                     </button>
-                    <button>
-                        {/* <i class="fas fa-eye-slash"></i> Not Showed Icon */}
-                        <i className="fas fa-eye" />
+                        <button className="noShowBtn">
+                            {/* <i class="fas fa-eye-slash"></i> Not Showed Icon */}
+                            <i className="fas fa-eye" />
                         No Show
                     </button>
                     </div>
@@ -35,5 +35,5 @@ const Appointment = props => {
         </React.Fragment>
     );
 };
- 
+
 export default Appointment;
--- src/components/center/center_appointments_List/center-appointments.jsx
+++ src/components/center/center_appointments_List/center-appointments.jsx
@@ -2,31 +2,31 @@ import React, { Component } from 'react';
 import AppointmentsTable from './appointments-table';
 
 class CenterAppointments extends Component {
-    state = {  }
-    render() { 
-        return ( 
-           <React.Fragment>
+    state = {}
+    render() {
+        return (
+            <React.Fragment>
                 <div className="container m-5 p-5 d-flex justify-content-between">
 
                     {/* Appointments Tabs */}
                     <div className="appointments-tabs div-border">
                         <ul>
-                        <li className="selected-tab">Current 
+                            <li className="selected-tab">Current
                             Appointments
                         </li>
-                        <hr className="m-0" />
-                        <li>Previous 
+                            <hr className="m-0" />
+                            <li>Previous
                             Appointments
                         </li>
                         </ul>
                     </div>
-                    
+
                     {/* Appoitntments Table */}
-                    <AppointmentsTable/>
+                    <AppointmentsTable />
                 </div>
-           </React.Fragment>
+            </React.Fragment>
         );
     }
 }
- 
+
 export default CenterAppointments;
