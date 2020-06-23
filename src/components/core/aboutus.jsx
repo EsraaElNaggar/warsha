a34e4cb4df197a326097c0543412a27fb4c443bb
EsraaElNaggar <esraamelnaggar@hotmail.com>
2020-06-24 00:02:38 +0200

    ✨ About us style

--- src/components/core/aboutus.jsx
+++ src/components/core/aboutus.jsx
@@ -2,16 +2,69 @@ import React from 'react';
 
 import Nav from './nav/nav';
 import Footer from './footer';
+import { Link } from 'react-router-dom';
 
 const AboutUs = props => {
     return (
         <React.Fragment>
 
             {/* Navbar */}
-            <Nav className="white"/>
+            <Nav className="white" />
+
+            {/* new About us  */}
+            {/* what is warsha?  */}
+            <section className="logWrap landWrap about">
+                <div className="logLeft aboutt land ">
+                    <Link to="/"><span>W</span>arsha</Link>
+
+                    {/* <h2><strong>Your Platform for </strong> <br /> <strong>Car Services</strong></h2> */}
+                    {/* <h2><strong> Welcome to our Website!</strong></h2> */}
+                    {/* <p className="pBook">Book online now !</p> */}
+                    <p className="aboutWarsha">Warsha is an E-commerce car services platform for different car users that allows to book car maintenance appointments, buy and sell spare parts  and buy, sell or rent cars</p>
+                </div>
+                <div className="aboutUs"></div>
+            </section>
+            <div className="miniALine"></div>
+            <section className="whoAreWe" >
+                <div className="logWrap landWrap team">
+                    <div className="logLeft aboutt land ">
+                        {/* <Link to="/"><span>W</span>arsha</Link> */}
+                        <h1>Who are we?</h1>
+                        <p className="aboutWarsha">We are a group of fresh graduate developer</p>
+                        <div className="flexii">
+                            <div>
+                                <div className="mo" ></div>
+                                <h3>Mona Mohamed</h3>
+                                <p>Web Developer</p>
+                            </div>
+                            <div>
+                                <div className="es"></div>
+                                <h3>Esraa ElNaggar</h3>
+                                <p>Front-End Developer</p>
+                            </div>
+
+                            <div>
+                                <div className="ha"></div>
+                                <h3>Mariam Hasan</h3>
+                                <p>Web Developer</p>
+                            </div>
+                            <div>
+                                <div className="ta"></div>
+                                <h3>Maryam Talal</h3>
+                                <p>Web Developer</p>
+                            </div>
+                        </div>
+                        {/* <h2><strong>Your Platform for </strong> <br /> <strong>Car Services</strong></h2> */}
+                        {/* <h2><strong> Welcome to our Website!</strong></h2> */}
+                        {/* <p className="pBook">Book online now !</p> */}
+                    </div>
+                    {/* <div className="aboutTeam"></div> */}
+                </div>
+            </section>
+
 
             {/* About Us Container */}
-            <section className="aboutus">
+            {/* <section className="aboutus">
                 <section className="Talal-banner">
                     <article className="Talal-banner-shape"></article>
                     <article className="Talal-banner-introduction">
@@ -63,10 +116,10 @@ const AboutUs = props => {
                         <div className="person-pic"><div></div></div>
                     </article>
                 </section>
-            </section>
+            </section> */}
 
             {/* Footer */}
-            <Footer/>
+            <Footer />
         </React.Fragment>
     );
 };
